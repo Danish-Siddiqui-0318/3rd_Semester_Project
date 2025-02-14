205b65fa9628f6336242dfcb392a0217e9345deb
Uzair <149260832+uzair-alt@users.noreply.github.com>
2025-02-15 03:14:00 +0500

    front end design work

--- Semester_Project/obj/Debug/net8.0/Semester_Project.AssemblyInfo.cs
+++ Semester_Project/obj/Debug/net8.0/Semester_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2ab38c1b15fec95387720ca8c69c463c57c5a1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8033392a6bbf975aebfc654dd086af6240b7d97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semester_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
